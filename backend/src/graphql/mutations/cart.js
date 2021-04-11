d74a2953e2e6d81f4a39c18c31d1d89864261ce9
TheNlesh <60070062@kmitl.ac.th>
2021-04-12 02:48:18 +0700

    Added Cart Update

--- backend/src/graphql/mutations/cart.js
+++ backend/src/graphql/mutations/cart.js
@@ -3,3 +3,4 @@ import { CartTC } from '../../models'
 export const updatePrice = CartTC.getResolver('updateById')
 export const addToCart = CartTC.getResolver('createOne')
 export const delProduct = CartTC.getResolver('removeOne')
+export const updateCart = CartTC.getResolver('updateOne')
