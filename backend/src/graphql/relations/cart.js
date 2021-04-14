15dbce9d614303cd538b929c73ebdc9a8870e127
TheNlesh <60070062@kmitl.ac.th>
2021-04-14 20:40:56 +0700

    Fixed Relation

--- backend/src/graphql/relations/cart.js
+++ backend/src/graphql/relations/cart.js
@@ -1,4 +1,6 @@
-import { CartTC, UserTC, productInCartTC, ProductTC } from '../../models'
+import { createSourceEventStream } from 'graphql'
+import { CartTC, UserTC, productInCartTC, BaseProductTC } from '../../models'
+import { productInCart } from '../queries/cart'
 
 CartTC.addRelation(
     'createdByUser',{
@@ -10,12 +12,22 @@ CartTC.addRelation(
     }
 )
 
-productInCartTC.addRelation(
-    'ProductInfo',{
-        resolver: () => ProductTC.getResolver('findById'),
+CartTC.getFieldTC('product').addRelation(
+    'productInfo',{
+        resolver: () => BaseProductTC.getResolver('findById'),
         prepareArgs: {
             _id: (source) => source.productId,
         },
         projection: {productId: true},
     }
 )
+
+// productInCartTC.addRelation(
+//     'productInfo',{
+//         resolver: () => BaseProductTC.getResolver('findById'),
+//         prepareArgs: {
+//             _id: (source) => source.productId,
+//         },
+//         projection: {productId: true},
+//     }
+// )
