fbb62ef58a251dfba21b67eafba6d1fe839d156a
Udomeak <60070120@it.kmitl.ac.th>
2021-04-18 15:34:32 +0700

    format document and edit variable

--- frontend/src/components/Checkout/Checkout.js
+++ frontend/src/components/Checkout/Checkout.js
@@ -1,89 +1,101 @@
-import { react, useState } from 'react'
-import { useQuery, useMutation } from '@apollo/client'
-import { CREATE_ORDER } from '../../graphql/createOrder'
-import { QUERY_CART, QUERY_CART_ORDER } from '../../graphql/CartQuery'
+import { react, useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { CREATE_ORDER } from "../../graphql/createOrder";
+import { QUERY_CART, QUERY_CART_ORDER } from "../../graphql/CartQuery";
 import { useHistory } from "react-router-dom";
-import { useSession } from '../../contexts/SessionContext'
-
-
+import { useSession } from "../../contexts/SessionContext";
 
 const Checkout = () => {
-    const { user } = useSession()
-    const {error, loading, data} = useQuery( QUERY_CART, {variables: {userId: user?._id}})
-    const {data: dataOrder} = useQuery(QUERY_CART_ORDER, {variables: {userId: user?._id}})
-    const [createOrder] = useMutation(CREATE_ORDER)
-    let history = useHistory();
+  const { user } = useSession();
+  const { error, loading, data } = useQuery(QUERY_CART, {
+    variables: { userId: user?._id },
+  });
+  const { data: dataOrder } = useQuery(QUERY_CART_ORDER, {
+    variables: { userId: user?._id },
+  });
+  const [createOrder] = useMutation(CREATE_ORDER);
+  let history = useHistory();
 
-    function createOrderBtn(){
+  function createOrderBtn() {
+    let dataOrderString = JSON.stringify(dataOrder);
+    let dataOrderJSON = JSON.parse(dataOrderString);
 
-        let eiei = JSON.stringify(dataOrder)
-        let newone = JSON.parse(eiei)
+    let a = [...dataOrderJSON.cart[0].product];
+    a.map((obj) => delete obj.__typename);
 
-        let a = [...newone.cart[0].product]
-        a.map((obj) => delete obj.__typename)
+    console.log("create order success");
+    history.push("/payment");
 
-        console.log("create order success")
-        history.push('/payment')
+    return createOrder({
+      variables: {
+        userId: "6077f2c070ffdf2acc072bc9",
+        statusOrder: "waiting",
+        payment: "unspecify",
+        product: a,
+      },
+    });
+  }
 
-        return createOrder({variables: {
-            userId: "6077f2c070ffdf2acc072bc9",
-            statusOrder: "waiting",
-            payment: "unspecify",
-            product: a
-        }})
-
-    }
-
-    if(user){
-        return(
-            <>
-
-            <div className="flex">
-                <div className="m-3 p-2 bg-red-200 ">
-                    {data?.cart[0]?.createdByUser?.firstName}{" "}{data?.cart[0]?.createdByUser?.lastName}
-                    {data?.cart[0].product.map((product)=>{
-                        return(
-                            <div className="m-1 p-2 bg-green-200">
-                                <ul>
-                                    <li key="productID">
-                                        <b>Productid</b><br />{product?.productId}
-                                    </li>
-                                    <li>
-                                        <b>name</b><br />{product?.productInfo?.name}
-                                    </li>
-                                    <li>
-                                        <b>Price</b><br />{product?.productInfo?.price}
-                                    </li>
-                                    <li>
-                                        <b>Stock</b><br />{product?.productInfo?.count}
-                                    </li>
-                                    <li>
-                                        <b>Quantity</b><br />{product?.quantity}
-                                    </li>
-                                </ul>
-                            </div>
-                        )
-                    })}
+  if (user) {
+    return (
+      <>
+        <div className="flex">
+          <div className="m-3 p-2 bg-red-200 ">
+            {data?.cart[0]?.createdByUser?.firstName}{" "}
+            {data?.cart[0]?.createdByUser?.lastName}
+            {data?.cart[0].product.map((product) => {
+              return (
+                <div className="m-1 p-2 bg-green-200">
+                  <ul>
+                    <li key="productID">
+                      <b>Productid</b>
+                      <br />
+                      {product?.productId}
+                    </li>
+                    <li>
+                      <b>name</b>
+                      <br />
+                      {product?.productInfo?.name}
+                    </li>
+                    <li>
+                      <b>Price</b>
+                      <br />
+                      {product?.productInfo?.price}
+                    </li>
+                    <li>
+                      <b>Stock</b>
+                      <br />
+                      {product?.productInfo?.count}
+                    </li>
+                    <li>
+                      <b>Quantity</b>
+                      <br />
+                      {product?.quantity}
+                    </li>
+                  </ul>
                 </div>
-            </div>
-            
+              );
+            })}
+          </div>
+        </div>
 
-            <button onClick={createOrderBtn} className="m-2 p-1 bg-gray-200 shadow-md hover:shadow-xl">Go to Payment</button>
+        <button
+          onClick={createOrderBtn}
+          className="m-2 p-1 bg-gray-200 shadow-md hover:shadow-xl"
+        >
+          Go to Payment
+        </button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="w-screen h-screen  text-2xl align-middle p-3">
+          {"Invalid Session, Please Login First!"}
+        </div>
+      </>
+    );
+  }
+};
 
-            </>
-        )
-    }
-
-    else{
-        return(
-            <>
-            <div className="w-screen h-screen  text-2xl align-middle p-3">
-                {"Invalid Session, Please Login First!"}
-            </div>
-            </>
-        )
-    }
-    
-}
-
-export default Checkout
+export default Checkout;
--- frontend/src/components/Order/Order.js
+++ frontend/src/components/Order/Order.js
@@ -1,64 +1,79 @@
-import { React } from 'react'
-import { useQuery } from '@apollo/client'
-import { QUERY_ORDER } from '../../graphql/queryOrder'
-import { useSession } from '../../contexts/SessionContext'
+import { React } from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_ORDER } from "../../graphql/queryOrder";
+import { useSession } from "../../contexts/SessionContext";
 
 const Order = () => {
-    const { user } = useSession()
-    const {error, loading, data} = useQuery( QUERY_ORDER, {variables: {userId: user?._id}} )
+  const { user } = useSession();
+  const { error, loading, data } = useQuery(QUERY_ORDER, {
+    variables: { userId: user?._id },
+  });
 
-    if(user){
-        return(
-            <>
-            {data && data.order.map((order)=>{
-                return(
-                    <div className="flex">
-                        <div className="m-3 p-2 bg-yellow-200 ">
-                            {order?.createdByUser?.firstName}{" "}{order?.createdByUser?.lastName}<br />
-                            <b>{"Status of Order: "}</b>{order?.status}<br />
-                            <b>{"Payment: "}</b>{order?.paymentDetail}<br />
-    
-                            {order?.product.map((product)=>{
-                                return(
-                                    <div className="m-1 p-2 bg-purple-200">
-                                        <ul>
-                                            <li key="productID">
-                                                <b>Productid</b><br />{product?.productId}
-                                            </li>
-                                            <li>
-                                                <b>name</b><br />{product?.productInfo?.name}
-                                            </li>
-                                            <li>
-                                                <b>Price</b><br />{product?.productInfo?.price}
-                                            </li>
-                                            <li>
-                                                <b>Stock</b><br />{product?.productInfo?.count}
-                                            </li>
-                                            <li>
-                                                <b>Quantity</b><br />{product?.quantity}
-                                            </li>                                        
-                                        </ul>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    </div>
-                )
-            })}
-            </>
-        )
-    }
+  if (user) {
+    return (
+      <>
+        {data &&
+          data.order.map((order) => {
+            return (
+              <div className="flex">
+                <div className="m-3 p-2 bg-yellow-200 ">
+                  {order?.createdByUser?.firstName}{" "}
+                  {order?.createdByUser?.lastName}
+                  <br />
+                  <b>{"Status of Order: "}</b>
+                  {order?.status}
+                  <br />
+                  <b>{"Payment: "}</b>
+                  {order?.paymentDetail}
+                  <br />
+                  {order?.product.map((product) => {
+                    return (
+                      <div className="m-1 p-2 bg-purple-200">
+                        <ul>
+                          <li key="productID">
+                            <b>Productid</b>
+                            <br />
+                            {product?.productId}
+                          </li>
+                          <li>
+                            <b>name</b>
+                            <br />
+                            {product?.productInfo?.name}
+                          </li>
+                          <li>
+                            <b>Price</b>
+                            <br />
+                            {product?.productInfo?.price}
+                          </li>
+                          <li>
+                            <b>Stock</b>
+                            <br />
+                            {product?.productInfo?.count}
+                          </li>
+                          <li>
+                            <b>Quantity</b>
+                            <br />
+                            {product?.quantity}
+                          </li>
+                        </ul>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="w-screen h-screen  text-2xl align-middle p-3">
+          {"Invalid Session, Please Login First!"}
+        </div>
+      </>
+    );
+  }
+};
 
-    else{
-        return(
-            <>
-            <div className="w-screen h-screen  text-2xl align-middle p-3">
-                {"Invalid Session, Please Login First!"}
-            </div>
-            </>
-        )
-    }
-    
-}
-
-export default Order
+export default Order;
