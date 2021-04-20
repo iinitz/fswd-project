8efd8813735f90cc8ead432282fce786a44e3f69
TheNlesh <60070062@kmitl.ac.th>
2021-04-20 22:52:10 +0700

    Add number of product in cart and add product to cart feature

--- frontend/src/components/Navigation/NavigationItems/NavigationItems.js
+++ frontend/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,6 +2,9 @@ import React, { useMemo, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
 import { useSession } from '../../../contexts/SessionContext'
+import { QUERY_CART } from '../../../graphql/CartQuery'
+import { useQuery } from '@apollo/client'
+
 
 import classes from './NavigationItems.module.css'
 
@@ -11,6 +14,7 @@ const NavigationItem = React.lazy(() =>
 
 const NavigationItems = (props) => {
   const { loading, user } = useSession()
+  const { data } = useQuery( QUERY_CART, {variables: {userId: user?._id}} )
 
   const userBox = useMemo(() => {
     if (loading) {
@@ -49,7 +53,13 @@ const NavigationItems = (props) => {
         Product
       </NavigationItem>
       <NavigationItem link="/cart" clickedFromNav={props.clicked} exact>
-        Cart
+        <span className="relative inline-block">
+          <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
+            {data?.cart[0]?.product?.length ?? 0}
+          </span>
+          Cart
+          {/* <img src="https://www.flaticon.com/svg/vstatic/svg/1170/1170627.svg?token=exp=1618931046~hmac=c087b263d3990714eac48cd42396667f" height="25" width="25"/> */}
+        </span>
       </NavigationItem>
       <NavigationItem link="/order" clickedFromNav={props.clicked} exact>
         Order
--- frontend/src/components/Product/Product.js
+++ frontend/src/components/Product/Product.js
@@ -2,10 +2,21 @@ import ProductCard from './ProductCard'
 
 import { DELETE_PRODUCT_MUTATION } from '../../graphql/deleteProductById'
 import { useCallback, useState } from 'react'
-import { useMutation } from '@apollo/client'
+import { QUERY_CART } from '../../graphql/CartQuery'
+import { UPDATE_CART } from '../../graphql/CartMutation'
+import { useSession } from '../../contexts/SessionContext'
+import { useMutation, useQuery } from '@apollo/client'
 const Product = (props) => {
     const {product} = props
     const [[deleteProduct]] = [useMutation(DELETE_PRODUCT_MUTATION)]
+    const { user } = useSession()
+    const { data } = useQuery( QUERY_CART, {variables: {userId: user?._id}} )
+    const refetchQuery = {refetchQueries: [{
+            query: QUERY_CART,
+            variables: {userId: user?._id}
+        }]
+    }
+    const [updateCart] = useMutation(UPDATE_CART, refetchQuery)
     const handleButtonClick = useCallback((e) =>  {
         try {
             console.log(product?._id)
@@ -15,6 +26,27 @@ const Product = (props) => {
             console.log(JSON.stringify(err))
         }
     }, [])
+
+    function addtoCart(productId){
+        var temp = JSON.stringify(data.cart[0].product)
+        var inCart = JSON.parse(temp)
+        
+        var newProduct = {
+            productId: productId,
+            quantity: 1
+          }
+        var productIndex = inCart.findIndex((item) => item.productId === productId)
+
+        if (productIndex === -1){
+            inCart = [...inCart, newProduct]
+        }else{
+            inCart[productIndex].quantity = Math.min(inCart[productIndex].quantity + 1, inCart[productIndex].productInfo.count)
+        }
+        inCart.map((obj) => delete obj.productInfo)
+        inCart.map((obj) => delete obj.__typename)
+        return updateCart({variables: {userId: user?._id, product: inCart}})
+    }
+
     return (
         <div>
             <div class="max-w-sm rounded overflow-hidden shadow-lg">
@@ -27,7 +59,7 @@ const Product = (props) => {
                 </div>
             </div>
             <div class="px-6 pt-4 pb-2">
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2" >Add to cart</span>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => addtoCart(product?._id)}> Add to cart</button>
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={handleButtonClick}>
                     Remove Product
                 </button>
